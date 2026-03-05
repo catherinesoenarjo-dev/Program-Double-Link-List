6ab2a679e4b2da2a309582b3ac5a364a084cf00e
catherinesoenarjo-dev <catherinesoenarjo@gmail.com>
2026-03-05 21:10:27 +0800

    Add files via upload

---
+++ joinInsertDelete.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data; // value
+    Node* next; // pointer
+};
+
+void insertFirst(Node* & head,int value){
+    Node* newNode = new Node;
+    newNode -> data = value;
+    newNode -> next = head;
+    head = newNode;
+};
+
+void insertNext(Node* prevNode,int value){
+    Node* newNode = new Node;
+    newNode -> data = value;
+    newNode -> next = prevNode->next;
+    prevNode->next = newNode;
+}
+
+void insertLast(Node* & head,int value){
+    Node* newNode = new Node;
+    newNode -> data = value;
+    newNode -> next = NULL;
+    
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next !=NULL){
+        temp = temp -> next;
+    }
+    temp ->next = newNode;
+}
+
+void deleteFirst(Node* & head){
+    Node* temp = head;
+    head = head -> next;
+    delete temp;
+}
+
+void deleteLast(Node* & head){
+    if (head == NULL){
+        return;
+    }
+    
+    if (head->next == NULL){
+        delete head;
+        head = NULL;
+        return;
+    }
+    Node* temp = head;
+
+    while (temp->next->next != NULL){
+        temp = temp->next;
+    } 
+
+    delete temp->next;     
+    temp->next = NULL;
+}
+
+int main (){
+    Node* head = NULL;
+    Node* node1 = new Node;
+    node1 -> data = 60;
+    node1 -> next = NULL;
+    head = node1;
+
+
+    Node* node2 = new Node;
+    node2 -> data = 75;
+    node2 -> next = NULL;
+    head -> next = node2;
+
+
+    Node* node3 = new Node;
+    node3 -> data = 90;
+    node3 -> next = NULL;
+    node2 -> next = node3;
+
+    insertFirst (head,50);
+    insertNext (node2,88);
+    insertLast (head,99);
+
+    deleteFirst (head);
+    deleteLast (head);
+
+    Node* temp = head;
+    while (temp !=NULL){
+        cout << temp -> data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+    return 0;
+}
